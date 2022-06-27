d73006ad69c2dce86b9e472adb04e4b8d8280d23
Leo <leoigornunes@protonmail.com>
2022-06-26 22:45:50 -0300

    FEAT(testes):teste para mover os arquivos

--- test/test_organization.py
+++ test/test_organization.py
@@ -29,7 +29,33 @@ class testClass(unittest.TestCase):
         """
         Deletando arquivos e pastas
         """
-    
+    def test_move_files(self):
+        """
+        Pegando variavel de ambiente home
+        deleta caso exista pasta temporaria
+        e criando pastas temporarias para teste
+        """
+        PATH = os.getenv('HOME')
+        if os.path.isdir(PATH+'/test') and os.path.isfile(PATH+'/testMovieFiles') :
+            shutil.rmtree(PATH+'/test/')
+            shutil.rmtree(PATH+'/testMovieFiles')
+        elif os.path.isdir(PATH+'/test'):
+            shutil.rmtree(PATH+'/test/')
+        elif os.path.isfile(PATH+'/testMovieFiles'):
+            shutil.rmtree(PATH+'/testMovieFiles')
+        os.mkdir(PATH+'/test')
+        os.mkdir(PATH+'/testMovieFiles')
+        """
+        Criando arquivo temporario de 
+        """
+        file = open(PATH+'/test/file.txt' , 'w+')
+        file.close
+        self.assertEqual(len(organization.listFiles(PATH+'/testMovieFiles')),0)
+        """
+        Movendo arquivo
+        """
+        organization.movies_files(PATH+'/test',PATH+'/testMovieFiles')
+        self.assertEqual(len(organization.listFiles(PATH+'/testMovieFiles')),1)
         
 
         
