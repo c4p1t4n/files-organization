e070ac96f146e94bf9971577ffc5345440fcb77f
Leo <leoigornunes@protonmail.com>
2022-07-13 03:57:53 -0300

    FIX(fileClass): move todos os arquivos no diretorio atual para o de destino

--- src/FileClass.py
+++ src/FileClass.py
@@ -4,33 +4,33 @@ import os
 import shutil
 class File:
     def __init__(self,extension:list,current_dir:str,destination_dir:str):
-        
         self.extension = list(extension)
-        self.extension = list(map(lambda x: x * 2 , self.extension))
+        self.extension = list(map(lambda x: x.replace('.','') , self.extension))
         self.current_dir = str(current_dir)
         self.destination_dir = str(destination_dir)
+
     def createDirectory(self,dir):
         try:
             os.mkdir(dir)
         except FileExistsError:
             self.delete_directory(dir)
             os.mkdir(dir)
+
     def delete_directory(self,dir):
         shutil.rmtree(dir)
 
     def list_files(self,path:str):
-        files_extension = list(filter(lambda x: (x.split('.')[-1] in self.extension) ,os.listdir(path)))
+        files_extension = list(filter(lambda x: (x.split('.')[-1] in self.extension) , os.listdir(path)))
         return files_extension
 
-
-    def movies_files(self,current_dir, destination_dir):
-        files = self.listFiles(current_dir)
-        for fileName in files:
-            print(f'moving file {fileName}')
+    def movies_files(self):
+        files = self.list_files(self.current_dir)
+        for filename in files:
+            print(f'moving file {filename}')
             try:
-                shutil.move(current_dir, destination_dir)
+                shutil.move(self.current_dir+filename,self.destination_dir)
                 print('successfully moved') 
             except FileNotFoundError:
-                print(f'fail in moved create directory {destination_dir}')
-                self.create_directory(destination_dir)
+                print(f'fail in moved. Create directory {self.destination_dir}')
+                self.create_directory(self.destination_dir)
     
