ae66f29ea60583e3dd1bae8ef3bc60707d4c30b2
Leo <leoigornunes@protonmail.com>
2022-07-13 03:00:21 -0300

    FEAT(list_files): listando somente os arquivos com extensao da classe

--- src/FileClass.py
+++ src/FileClass.py
@@ -1,3 +1,4 @@
+from attr import s
 from importlib_metadata import files
 import os
 import shutil
@@ -5,9 +6,9 @@ class File:
     def __init__(self,extension:list,current_dir:str,destination_dir:str):
         
         self.extension = list(extension)
+        self.extension = list(map(lambda x: x * 2 , self.extension))
         self.current_dir = str(current_dir)
         self.destination_dir = str(destination_dir)
-
     def createDirectory(self,dir):
         try:
             os.mkdir(dir)
@@ -17,10 +18,12 @@ class File:
     def delete_directory(self,dir):
         shutil.rmtree(dir)
 
-    def listFiles(self,path:str):
-        return os.listdir(path)
-        
-    def moviesFiles(self,current_dir, destination_dir):
+    def list_files(self,path:str):
+        files_extension = list(filter(lambda x: (x.split('.')[-1] in self.extension) ,os.listdir(path)))
+        return files_extension
+
+
+    def movies_files(self,current_dir, destination_dir):
         files = self.listFiles(current_dir)
         for fileName in files:
             print(f'moving file {fileName}')
